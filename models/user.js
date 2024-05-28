b7956b08f385891d2a54ea1529e2905d4c667f3d
Jackson Raymond <jackson.raymond07@gmail.com>
2024-05-28 11:57:50 +1000

    add and remove functions

--- models/user.js
+++ models/user.js
@@ -4,17 +4,17 @@ const {lineupSchema, test} = require('./lineup')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema ({
-      name: String,
-      googleId: {
-        type: String,
-        required: true
-      },
-      email: String,
-      avatar: String,
-      lineup: [test] 
-    }, {
+    name: String,
+    googleId: {
+      type: String,
+      required: true
+    },
+    email: String,
+    avatar: String,
+    lineup: [test] 
+  }, {
 
-      timestamps: true,
+    timestamps: true,
       
 })
 
